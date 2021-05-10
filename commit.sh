478a439edd614fa33b37d2153f0e54d197f00ca5
ppzzyy11 <q15138956968@gmail.com>
2021-05-10 12:11:09 +0800

    .

---
+++ commit.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+# commit.sh
+# Copyright (C) 2021 pzy <pzy@pzy-B250M-Wind>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+git add *cpp template/*cpp summary todo statistic
+git commit -am. 
+git push
